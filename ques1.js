86b200d1900cf7a418300789e1106705e9d623ea
Raj1903 <57721977+Raj1903@users.noreply.github.com>
2021-08-18 17:44:10 +0530

    Create ques1.js
    By using the( getMilliseconds) method milliseconds can be fetch from the given date

---
+++ ques1.js
@@ -0,0 +1,6 @@
+
+//How to fetch the millisecond according to universal time from a given Date object?
+
+var dateToday = new Date('October 15, 1996 05:35:32:77 GMT+11:00');
+console.log(dateToday)                         //it will show the defined date
+console.log(dateToday.getMilliseconds());   // .getMilliseconds is keyword is used to fetch the milli seconds from given date
